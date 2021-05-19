9e277fb839e1ae23dc3f8e8faff74c3fecdea531
namisunflower <namisunflower@byui.edu>
2021-05-20 00:19:10 +0800

    updated

--- src/swarts/IntroduceMyself.java
+++ src/swarts/IntroduceMyself.java
@@ -3,15 +3,23 @@ package swarts;
 import java.awt.*;
 
 public class IntroduceMyself {
-
+    private String collaboratorName;
     private String name;
 
     public String getName() {
         return name;
     }
 
+    public String getCollaboratorName() {
+        return collaboratorName;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setCollaboratorName(String collaboratorName) {
+        this.collaboratorName = collaboratorName;
     }
 
     public IntroduceMyself(String name) {
@@ -22,8 +30,12 @@ public class IntroduceMyself {
         System.out.println("Hello my name is " + getName());
     }
 
+    public void replyBack(){System.out.println("Hello " + getName() + ", my name is " + getCollaboratorName() + ".");}
+
     public static void main(String[] args) {
         IntroduceMyself greeting = new IntroduceMyself("Dallin");
         greeting.sayHello();
+        greeting.setCollaboratorName("Frances");
+        greeting.replyBack();
     }
 }
